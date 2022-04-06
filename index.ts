7ad14bd2e708040da4b4e9463247482b845fa3f0
mxinburritos <mxinburritos@gmail.com>
2022-04-06 01:32:06 -0400

    Change build script

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import concurrently from "concurrently";
 
 concurrently([
-  { command: "cd gateway/src && node index.js", name: "gateway" },
-  { command: "cd services/posts/src && node app.js", name: "posts" },
-  { command: "cd services/tasks/src && node app.js", name: "tasks" },
+  { command: "cd dist/gateway/src && node index.js", name: "gateway" },
+  { command: "cd dist/services/posts/src && node app.js", name: "posts" },
+  { command: "cd dist/services/tasks/src && node app.js", name: "tasks" },
 ]);
