2c79e8e4f752b537dc0bdf1bb15b60d631ea9c44
mxinburritos <mxinburritos@gmail.com>
2022-04-06 00:02:52 -0400

    Change build settings

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ import concurrently from "concurrently";
 concurrently([
   { command: "cd gateway && yarn dev", name: "gateway" },
   { command: "cd services/posts && yarn dev", name: "posts" },
-  { command: "cd services/tasks && yarn dev", name: "tasks" },
+  { command: "cd services/tasks && yarn start", name: "tasks" },
 ]);
